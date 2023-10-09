8c0ec5b2c4bcb3eb23c37d19659b01d24eb92b72
jessy413 <jessy413@ucla.edu>
2023-10-09 12:25:38 -0700

    Draw bounding box and find dominant color

---
+++ video.py
@@ -0,0 +1,85 @@
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+
+def find_histogram(clt):
+    """
+    create a histogram with k clusters
+    :param: clt
+    :return:hist
+    """
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    return hist
+def plot_colors2(hist, centroids):
+    bar = np.zeros((50, 300, 3), dtype="uint8")
+    startX = 0
+
+    for (percent, color) in zip(hist, centroids):
+        # plot the relative percentage of each cluster
+        endX = startX + (percent * 300)
+        cv.rectangle(bar, (int(startX), 0), (int(endX), 50),
+                      color.astype("uint8").tolist(), -1)
+        startX = endX
+
+    return bar
+
+
+cap = cv.VideoCapture(0)
+while(1):
+    # Take each frame
+    _, frame = cap.read()
+    # Convert BGR to HSV
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    # define range of blue color in HSV
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
+    # Threshold the HSV image to get only blue colors
+    mask = cv.inRange(hsv, lower_blue, upper_blue)
+    # Bitwise-AND mask and original image
+    res = cv.bitwise_and(frame,frame, mask= mask)
+    #cv.imshow('frame',frame)
+    #cv.imshow('mask',mask)
+    #cv.imshow('res',res)
+
+    # Bounding Box
+    '''
+    gray = cv.cvtColor(res, cv.COLOR_BGR2GRAY)
+
+    ret,thresh = cv.threshold(gray,127,255,0)
+    contours,hierarchy = cv.findContours(thresh, 1,2)
+
+    if contours:
+        cnt = contours[0] 
+        x,y,w,h = cv.boundingRect(cnt)
+        cv.rectangle(res,(x,y),(x+w,y+h),(0,255,0),2)
+    cv.imshow('res',res)
+    '''
+
+    # Dominant Color
+    
+    img = frame.reshape((frame.shape[0] * frame.shape[1],3)) #represent as row*column,channel number
+    clt = KMeans(n_clusters=3) #cluster number
+    clt.fit(img)
+
+    hist = find_histogram(clt)
+    bar = plot_colors2(hist, clt.cluster_centers_)
+
+    plt.axis("off")
+    plt.imshow(bar)
+    plt.show()
+    
+    #cv.imshow('frame',frame)
+
+    #plt.waitKey()
+    k = cv.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv.destroyAllWindows()
